e8c98d95796db77778e0e8b0f4af05c9804b8b08
Ema <Ema@test.com>
2020-12-14 16:16:39 +0200

    Added Saucedemo Basic Functionalities Test Suite.

---
+++ SauceDemoTests/Test Suites/Saucedemo Basic Functionalities.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Basic functionalities for Saucedemo platform.</description>
+   <name>Saucedemo Basic Functionalities</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4da5d806-70ed-4f1b-81f8-900bd9416df6</testSuiteGuid>
+   <testCaseLink>
+      <guid>8020ae46-fbcf-4f92-8764-941bea28ef6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Logout</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8705d43-ddf0-4517-ab9e-081d43a379c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add_to_cart</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41d48852-f201-4b0f-a3ea-f201d90fbbd3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add_to_cart_multiple_items</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
