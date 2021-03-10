6c9baf9794a2a31f383e96d2d03314888cfe424b
Ema <Ema@test.com>
2021-03-10 15:14:13 +0200

    Added AutomationPractice Basic Functionalities Test Suite. Added Login_Logout test case to this Test Suite.

---
+++ AutomationPractice Platform Tests/Test Suites/AutomationPractice Basic Functionalities.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AutomationPractice Basic Functionalities</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0296979f-bce7-4917-bc6e-a82029322325</testSuiteGuid>
+   <testCaseLink>
+      <guid>44a3ea69-0ab5-4b2c-bf3f-c7d46bde3632</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
